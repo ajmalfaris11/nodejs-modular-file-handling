b5ff4caaa5a9fc775910019773618889fa43a414
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-13 01:24:48 +0530

    Add functionality to append content to 'data.txt' with newline character

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ const fs = require("fs")
 
 //  writing a file
 
-fs.writeFile('data.txt', 'Hello world', function(err) {
-    if(err) throw err ;
-    console.log('file written')
-})
+// fs.writeFile('data.txt', 'Hello world', function(err) {
+//     if(err) throw err ;
+//     console.log('file written')
+// })
+
+
+
+
+// Import the built-in File System (fs) module
+const fs = require('fs');
+
+// Append "New Content" with a newline to 'data.txt' file
+fs.appendFile('data.txt', 'New Content \r\n', function (err) {
+    
+    // Check for any errors during the file append operation
+    if (err) throw err;
+    
+    // Log a success message when content is successfully added
+    console.log('Content added to file');
+});
+
