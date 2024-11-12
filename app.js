f0daf6db7bee497ef887efb4e62e7baf2ad22582
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-12 19:21:41 +0530

    feat: add file writing functionality with fs.writeFile

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 // File reading and writing
 const fs = require("fs")
 
+//  writing a file
+
+fs.writeFile('data.txt', 'Hello world', function(err) {
+    if(err) throw err ;
+    console.log('file written')
+})
